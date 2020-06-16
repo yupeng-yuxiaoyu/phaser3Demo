75ca558c5e5b77a7525a528d23f2328d25666c3e
yupeng1 <yupeng1@100tal.com>
2020-06-17 00:31:53 +0800

    feat: 添加开始页面的动画效果，及开始按钮

--- plane/src/index.js
+++ plane/src/index.js
@@ -8,6 +8,20 @@ gameSenceCenter.boot = {
   preload() {
     for (const key in assetsMap) {
       if (assetsMap.hasOwnProperty(key)) {
+        if (key === 'myplane') {
+          this.load.spritesheet(key, require(`./${assetsMap[key]}`), {
+            frameWidth: 40,
+            frameHeight: 40,
+          });
+          continue;
+        }
+        if (key === 'startbutton') {
+          this.load.spritesheet(key, require(`./${assetsMap[key]}`), {
+            frameWidth: 100,
+            frameHeight: 40,
+          });
+          continue;
+        } 
         assetsMap[key] = this.load.image(key, require(`./${assetsMap[key]}`));
       }
     }
@@ -21,7 +35,7 @@ gameSenceCenter.boot = {
         }
       })
       .setOrigin(0.5, 0.5);
-    if (!this.game.device.desktop) {
+    if (!this.game.device.os.desktop) {
       this.scale.scaleMode = Phaser.Scale.FIT;
       this.scale.refresh();
     }
@@ -42,7 +56,35 @@ gameSenceCenter.boot = {
 gameSenceCenter.start = {
   key: 'start',
   create() {
-    this.add.image(0, 0, 'bg').setOrigin(0);
+
+    // 添加背景
+    const bg = this.add.image(0, 0, 'bg').setOrigin(0);
+    this.add.image(this.game.config.width / 2, this.game.config.height - 16, 'copyright');
+
+    // 引入飞机精灵
+    const plane = this.add.sprite(this.game.config.width / 2, 100, 'myplane');
+
+    // 创建飞行帧动画
+    this.anims.create({
+      key: 'fly',
+      frames: this.anims.generateFrameNumbers('myplane', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // 飞机调用飞行动画
+    plane.anims.play('fly');
+
+    // 添加开始按钮
+    const startButton =  this.add.sprite(this.game.config.width / 2, 200, 'startbutton', 1).setInteractive();
+    startButton.on('pointerdown', () => {
+      startButton.setFrame(0);
+    })
+    startButton.on('pointerup', () => {
+      startButton.setFrame(1);
+      console.log('start game');
+    })
+   
   },
   update() {},
 }
