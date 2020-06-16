9bed3d025ed3a4337a4785064ec02a3b59b0a04a
yupeng1 <yupeng1@100tal.com>
2020-06-16 19:36:20 +0800

    feat: 切换为进度加载和场景两个层，添加资源加载进度

--- plane/src/index.js
+++ plane/src/index.js
@@ -1,31 +1,53 @@
 import Phaser from 'phaser';
 const assetsMap = require('./asset_map.json');
+
+const gameSenceCenter = {};
+
+gameSenceCenter.boot = {
+  key: 'boot',
+  preload() {
+    for (const key in assetsMap) {
+      if (assetsMap.hasOwnProperty(key)) {
+        assetsMap[key] = this.load.image(key, require(`./${assetsMap[key]}`));
+      }
+    }
+    const percentText = this.make.text({
+      x: this.game.config.width / 2,
+      y: this.game.config.height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    })
+    .setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+      percentText.setText(parseInt(value * 100) + '%');
+    });
+
+    this.load.on('complete', function () {
+      percentText.destroy();
+    });
+  },
+  create() {
+    this.scene.start('start');
+  },
+  update() {}
+}
+
+gameSenceCenter.start = {
+  key: 'start',
+  create() {
+    this.add.image(0,0,'bg').setOrigin(0);
+  },
+  update() {},
+}
+
 const config = {
   type: Phaser.AUTO,
   width: 240,
   height: 400,
-  scene: {
-    preload,
-    create,
-    update,
-  },
+  scene: [gameSenceCenter.boot, gameSenceCenter.start],
 };
-
 const game = new Phaser.Game(config);
-const asset = {};
-function preload() {
-  for (const key in assetsMap) {
-    if (assetsMap.hasOwnProperty(key)) {
-      asset[key] = this.load.image(key, require(`./${assetsMap[key]}`));
-    }
-  }
-}
-
-function create() {
-  const bg = this.add.image(0, 0, 'bg');
-  bg.setOrigin(0, 0);
-}
-
-function update() {
-
-}
